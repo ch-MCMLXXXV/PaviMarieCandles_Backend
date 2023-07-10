e9911bd0504285283d775aa0cb180df7683cc089
Courtney Harris <cjharris1985@gmail.com>
2023-07-10 16:13:48 -0500

    added inventory and location modules

--- medusa-config.js
+++ medusa-config.js
@@ -108,6 +108,12 @@ const modules = {
          redisUrl: REDIS_URL,
       },
    },
+   inventoryService: {
+      resolve: '@medusajs/inventory',
+   },
+   stockLocationService: {
+      resolve: '@medusajs/stock-location',
+   },
 };
 
 /** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
